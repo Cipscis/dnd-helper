8dd95b6bef971d24aa0057e8957bb89ba3c3c1ab
Mark Hanna <cipscis+github@gmail.com>
2017-08-23 12:05:24 +1200

    Added the ability to paste a URL or drag an image into the image control helper

--- assets/js/app/image/control.js
+++ assets/js/app/image/control.js
@@ -1,17 +1,45 @@
 define(
 	[
-		'jquery'
+		'jquery',
+		'templayed',
+
+		'text!templates/image-control.html'
 	],
 
-	function ($) {
+	function ($, templayed, imageControlTemplate) {
+
 		var ImageControl = {
 			init: function () {
 				ImageControl._initEvents();
+				ImageControl._loadIndex(ImageControl._renderImages);
 			},
 
 			_initEvents: function () {
 				$(document)
-					.on('click', '.js-image-control', ImageControl._selectImage);
+					.on('click', '.js-image-control', ImageControl._selectImage)
+
+					.on('dragover', '.js-image-drop', ImageControl._dragOver)
+					.on('drop', '.js-image-drop', ImageControl._drop)
+
+					.on('change', '.js-image-path', ImageControl._changePath);
+			},
+
+			_loadIndex: function (callback) {
+				var url = '/assets/json/image-control/index.json';
+
+				$.ajax({
+					url: url,
+					dataType: 'json',
+					complete: callback
+				});
+			},
+
+			_renderImages: function (data, statusCode) {
+				if (statusCode === 'success') {
+					$('.js-image-container').html(templayed(imageControlTemplate)(data.responseJSON));
+				} else {
+					console.error(arguments);
+				}
 			},
 
 			_selectImage: function (e) {
@@ -20,11 +48,56 @@ define(
 					imagePath = $image.attr('src'),
 					colour = $image.data('colour') || '#000';
 
-				$('.js-image-control.is-selected').removeClass('is-selected');
-				$wrap.addClass('is-selected');
+				ImageControl._setImage(imagePath, colour);
+			},
+
+			_setImage: function (imagePath, colour) {
+				imagePath = imagePath || '';
+				colour = colour || '#000000';
+
+				// Remove first to ensure an update is detected even
+				// if the same image has been selected again
+				localStorage.removeItem('image-display');
+				localStorage.removeItem('image-display-colour');
 
 				localStorage.setItem('image-display', imagePath);
 				localStorage.setItem('image-display-colour', colour);
+			},
+
+			_dragOver: function (e) {
+				e.preventDefault();
+			},
+
+			_drop: function (e) {
+				e.preventDefault();
+
+				var dataTransfer = e.originalEvent.dataTransfer,
+					image,
+					fileReader;
+
+				if (!dataTransfer.files.length) {
+					return;
+				}
+
+				image = dataTransfer.files[0];
+
+				fileReader = new FileReader();
+				fileReader.onload = ImageControl._dropImageLoad;
+				fileReader.readAsDataURL(image);
+			},
+
+			_dropImageLoad: function (e) {
+				var fileReader = e.target,
+					dataUrl = fileReader.result;
+
+				ImageControl._setImage(dataUrl);
+			},
+
+			_changePath: function (e) {
+				var $path = $(e.target).closest('.js-image-path'),
+					path = $path.val();
+
+				ImageControl._setImage(path);
 			}
 		};
 
